42fe0fc0d77eda22058b6227c56a25cae6254eb9
abdou <abdouashrat123@gmail.com>
2025-11-21 21:32:22 +0200

    feat: Define application constants and theme, and update dependencies

--- lib/core/app_const.dart
+++ lib/core/app_const.dart
@@ -1,5 +1,11 @@
+import 'dart:ui';
+
 abstract class AppConst {
   static const String appName = "SysGuard";
   static const String appVersion = "1.0.0";
   static const String baseUrl = "https://jsonplaceholder.typicode.com";
+  static const Color primaryColor = Color(0xFF1f2229);
+  static const Color warrningBackgroundColor = Color(0xFF363024);
+  static const Color warrningColor = Color(0xFFffaa00);
+  static const Color indicatorColor = Color(0xFF56d179);
 }
--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -1,1 +1,19 @@
-class AppTheme {}
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData mainTheme = ThemeData(
+    textTheme: const TextTheme(
+      headlineMedium: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+      ),
+      headlineLarge: TextStyle(
+        fontSize: 48,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+      ),
+      bodyMedium: TextStyle(fontSize: 14, color: Colors.white70),
+    ),
+  );
+}

771e38c32d69909fb087f6e523ef42cfe127822e
abdou <abdouashrat123@gmail.com>
2025-11-21 19:06:10 +0200

    feat: Add core application constants, theme, and initial page structure

---
+++ lib/core/app_const.dart
@@ -0,0 +1,5 @@
+abstract class AppConst {
+  static const String appName = "SysGuard";
+  static const String appVersion = "1.0.0";
+  static const String baseUrl = "https://jsonplaceholder.typicode.com";
+}
